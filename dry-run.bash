0aa7b5233a41c05827d09bb81bbbaabcefddb79a
killerkenobi <anthonyal@protonmail.com>
2023-12-17 14:54:52 -0500

    adjusting output in dry-run.bash

--- dry-run.bash
+++ dry-run.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Additional dependancies that will be uninstalled with tesseract..."
+echo "-----------------------"
 brew deps tesseract
-echo "This is a list of all the tesseract dependencies that will be uninstalled if you run the 'make clean' command."
-echo "When manually uninstalling tesseract via brew, you can choose to use the --ignore-dependencies flag to only uninstall"
-echo "tesseract while leaving its listed dependencies on your machine by running 'brew uninstall --ignore-dependencies tesseract'"
-
+echo "-----------------------"
+printf "When manually uninstalling tesseract via brew, you can choose to use the --ignore-dependencies flag to only uninstall
+tesseract while leaving its listed dependencies on your machine by running 'brew uninstall --ignore-dependencies tesseract' \n"
 
 echo "For additional dependencies that will be uninstalled when 'make clean' is run see requirements.txt."
